f752b271c26fa6c338faa5599604efcc05c0b118
latelier22 <latelier.cyrille@gmail.com>
2024-08-30 10:04:53 +0200

    port dans .env

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ const voiceID = "a5n9pJUnAhX4fn7lx3uo";
 const app = express();
 app.use(express.json());
 app.use(cors());
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
